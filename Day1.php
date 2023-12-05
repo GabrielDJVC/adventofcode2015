530ff52a7149ac90ae0d987eaf37cfd2ca226342
GabrielDJVC <djmgabriel@outlook.com>
2023-12-05 17:46:28 +0100

    commit

--- Day1.php
+++ Day1.php
@@ -3,19 +3,33 @@
 
 include_once("Utilities/Utilities.php");
 
-
 $arr_payload = openFile("Files/day1.txt");
 
-
-
-$int_nb_opened_par = substr_count($arr_payload[0],"(");
-
-$int_nb_closed_par = substr_count($arr_payload[0],")");;
-
+$int_nb_opened_par = substr_count($arr_payload[0], "(");
+$int_nb_closed_par = substr_count($arr_payload[0], ")");;
 $int_floors = $int_nb_opened_par - $int_nb_closed_par;
 
+
+$int_basement = 0;
+for ($i = 1; $i < strlen($arr_payload[0]); $i++) {
+
+    $str_new .= $arr_payload[0][$i];
+    if ($arr_payload[0][$i] == "(") {
+        $int_basement += 1;
+    }elseif($arr_payload[0][$i] == ")"){
+        $int_basement -= 1;
+    }
+  
+    
+    if($int_basement == -1){
+        break;
+
+    }
+}
 
 echo '<pre> ';
 var_dump($int_floors);
 echo '</pre>';
-
+echo '<pre> ';
+var_dump($i);
+echo '</pre>';
