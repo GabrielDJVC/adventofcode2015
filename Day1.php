ccb7ef2bd1ca538a3256d8fafe4a26090392f12a
GabrielDJVC <djmgabriel@outlook.com>
2023-12-05 19:52:44 +0100

    commit

--- Day1.php
+++ Day1.php
@@ -9,21 +9,18 @@ $int_nb_opened_par = substr_count($arr_payload[0], "(");
 $int_nb_closed_par = substr_count($arr_payload[0], ")");;
 $int_floors = $int_nb_opened_par - $int_nb_closed_par;
 
-
 $int_basement = 0;
-for ($i = 1; $i < strlen($arr_payload[0]); $i++) {
+for ($i = 0; $i < strlen($arr_payload[0]); $i++) {
 
     $str_new .= $arr_payload[0][$i];
     if ($arr_payload[0][$i] == "(") {
         $int_basement += 1;
-    }elseif($arr_payload[0][$i] == ")"){
+    } elseif ($arr_payload[0][$i] == ")") {
         $int_basement -= 1;
     }
-  
-    
-    if($int_basement == -1){
-        break;
 
+    if ($int_basement == -1) {
+        break;
     }
 }
 
@@ -31,5 +28,5 @@ echo '<pre> ';
 var_dump($int_floors);
 echo '</pre>';
 echo '<pre> ';
-var_dump($i);
+var_dump($i + 1);
 echo '</pre>';
