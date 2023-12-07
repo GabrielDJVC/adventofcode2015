cf318ed36204d69e805b8268015e6eed9b889441
GabrielDJVC <djmgabriel@outlook.com>
2023-12-07 18:42:08 +0100

    commit

--- Day2.php
+++ Day2.php
@@ -5,6 +5,7 @@ include_once("Utilities/Utilities.php");
 $arr_payload = openFile("Files/day2.txt");
 
 $total_paper = 0;
+$total_ribbon = 0;
 
 foreach ($arr_payload as $value) {
 
@@ -19,9 +20,17 @@ $arr_area = [
 $min_area = min($arr_area) / 2;
 
 $total_paper +=  $arr_area[0] + $arr_area[1] + $arr_area[2] + $min_area;
-    
+
+$arr_dims = array($l,$w,$h);
+sort($arr_dims);
+
+$total_ribbon += (2*$arr_dims[0]) + (2*$arr_dims[1]) + ($l*$w*$h);
+
 }
 
 echo '<pre> ';
 var_dump($total_paper);
+echo '</pre>';
+echo '<pre> ';
+var_dump($total_ribbon);
 echo '</pre>';
