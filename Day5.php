ce6d4998ed15b54e6c2da7f5b87023c5224bd489
GabrielDJVC <djmgabriel@outlook.com>
2023-12-22 14:42:24 +0100

    Partie 1

--- Day5.php
+++ Day5.php
@@ -3,24 +3,43 @@ include_once("Utilities/Utilities.php");
 
 $arr_payload = openFile("Files/day5.txt");
 
-$nb_vowels = 0;
 
+$regex = "(ab|cd|pq|xy)";
 $vowels = "aeiou";
 
-$two_letters = "";
+$answer1 = 0;
 
 foreach ($arr_payload as $value) {
 
+    $arr_nice_string = [
+        "vowels"  => false,
+        "letters" => false,
+        "string"  => false,
+    ];
+    $nice    = false;
+
+    $nb_vowels = 0;
     for ($i = 0; $i < strlen($value); $i++) {
 
         if (str_contains($vowels, $value[$i])) {
             $nb_vowels++;
+            if($nb_vowels == 3) $arr_nice_string['vowels'] = true;
         }
+        
+        $arr_double_letter = [];
+        $arr_double_letter[0] = $value[$i];
+        $arr_double_letter[1] = $value[$i + 1];
 
-        echo '<pre> ';
-        var_dump($arr_payload[0][$i]);
-        echo '</pre>';
+        if($arr_double_letter[0] == $arr_double_letter[1]) $arr_nice_string['letters'] = true; 
+
     }
+    
+    if(!preg_match($regex, $value))$arr_nice_string['string']=true;
 
-    exit;
+    if( $arr_nice_string['vowels'] == true && $arr_nice_string['letters'] == true && $arr_nice_string['string'] == true)  $nice=true;
+
+    if ($nice == true) $answer1++;
+    
 }
+
+echo '<pre> <strong>Partie 1</strong> : ' . $answer1 . '</pre> ';
